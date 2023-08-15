7f759ddb73abdcab0a01eec6e743e4bb95463f1c
roniragilimankhoirul <ronisensei7@gmail.com>
2023-08-15 22:58:48 +0700

    menambahkan unit test register users

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,7 @@
 import supertest from "supertest";
 import { app } from "../src/application/app.js";
 import { prismaClient } from "../src/application/database.js";
+import { logger } from "../src/application/logging.js";
 
 describe("POST /api/users", () => {
   afterEach(async () => {
@@ -27,6 +28,29 @@ describe("POST /api/users", () => {
       email: "",
       password: "",
     });
+
+    logger.info(result);
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined;
+  });
+  it("should cannot register duplicate user", async () => {
+    let result = await supertest(app).post("/api/users").send({
+      name: "Roni Ragil Iman Khoirul",
+      email: "example@gmail.com",
+      password: "12345678",
+    });
+    expect(result.status).toBe(200);
+    expect(result.body.data.name).toBe("Roni Ragil Iman Khoirul");
+    expect(result.body.data.email).toBe("example@gmail.com");
+    expect(result.body.data.password).toBeUndefined;
+
+    result = await supertest(app).post("/api/users").send({
+      name: "Roni Ragil Iman Khoirul",
+      email: "example@gmail.com",
+      password: "12345678",
+    });
+
+    logger.info(result.body);
     expect(result.status).toBe(400);
     expect(result.body.errors).toBeDefined;
   });
