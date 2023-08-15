40057fbfadcd48f8eaf4a48c5258a095d6f2d429
roniragilimankhoirul <ronisensei7@gmail.com>
2023-08-15 19:41:49 +0700

    setup express,winston,prisma

---
+++ src/application/database.js
@@ -0,0 +1,37 @@
+import prismaClient from "@prisma/client";
+import { logger } from "../application/logging";
+
+const prismaClient = PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
+
+prismaClient.$on("error", (e) => {
+  logger.error(e);
+});
+prismaClient.$on("warn", (e) => {
+  logger.warn(e);
+});
+prismaClient.$on("info", (e) => {
+  logger.info(e);
+});
+prismaClient.$on("query", (e) => {
+  logger.info(e);
+});
+module.exports = { prismaClient };
