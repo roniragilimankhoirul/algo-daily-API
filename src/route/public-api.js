549db67efa9d595fad58e3d1df235d067a01d442
roniragilimankhoirul <ronisensei7@gmail.com>
2023-09-26 23:30:11 +0700

    menambahakan Home endpoint GET /

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -1,6 +1,13 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
 const publicRouter = new express.Router();
+
+publicRouter.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello Algostudio",
+  });
+});
+
 publicRouter.post("/api/users", userController.register);
 publicRouter.post("/api/users/login", userController.login);
 
